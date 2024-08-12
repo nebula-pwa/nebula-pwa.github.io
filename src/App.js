d7a17f702a63d21acaa1d1f5d4062f0f96ff34d7
Thebatman7 <rembrand.pardo@gmail.com>
2024-08-12 12:59:13 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
-import NotFound from './components/NotFound'; // Ensure this component exists
+import NotFound from './components/NotFound'; // Ensure you have this component
 import './styles/App.css'; // Global styling
 
 function App() {
