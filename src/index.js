5e3e75f0ecd8da316ce0cb2dbf3cdd460f5362a0
Thebatman7 <rembrand.pardo@gmail.com>
2024-08-12 12:55:29 -0600

    update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
 import './styles/index.css'; // Import global styles if you have any
-import App from './App'; // Import your main App component
-import reportWebVitals from './reportWebVitals'; // Optional for performance monitoring
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router basename="/">
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
