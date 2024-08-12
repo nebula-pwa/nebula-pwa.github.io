5eed15eedc457b39d6238893bac037da58667968
Thebatman7 <rembrand.pardo@gmail.com>
2024-08-12 11:31:50 -0600

    update to improve urls

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
