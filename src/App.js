20bd1b3ae180c795d16b50c6586b34698e384716
Thebatman7 <rembrand.pardo@gmail.com>
2024-08-12 11:51:52 -0600

    update to get rid of # in url

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
+import NotFound from './components/NotFound';
 import './styles/App.css'; // Global styling
-
-// Add the NotFound component for handling 404 errors
-function NotFound() {
-  return <h1>404: Page Not Found</h1>;
-}
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <Navbar />
       <div className="content">
         <Routes>
