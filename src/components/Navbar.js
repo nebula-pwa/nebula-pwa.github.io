710c7e24b77edb5140f098533993c28475f407c0
Thebatman7 <rembrand.pardo@gmail.com>
2024-08-16 11:03:47 -0600

    update to nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,3 @@
-// import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import '../styles/Navbar.css'; // Import the CSS file
-
-// function Navbar() {
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   const toggleMenu = () => setIsOpen(!isOpen);
-
-//   return (
-//     <nav className="navbar">
-//       <div className="navbar-left">
-//         <img src="/assets/logo192.png" alt="Logo" className="logo" />
-//       </div>
-//       <button className="menu-toggle" onClick={toggleMenu}>
-//         ☰
-//       </button>
-//       <ul className={`navbar-right ${isOpen ? 'open' : ''}`}>
-//         <li><Link to="/">Home</Link></li>
-//         <li><Link to="/about">About</Link></li>
-//       </ul>
-//     </nav>
-//   );
-// }
-
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaBars, FaInfoCircle, FaEnvelope } from 'react-icons/fa'; // Import icons
@@ -38,18 +13,11 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <div className="navbar-left">
-        <Link to="/about">
-          <FaInfoCircle className="icon" />
-          About
-        </Link>
-        <Link to="/contact">
-          <FaEnvelope className="icon" />
-          Contact
-        </Link>
-      </div>
+      
       <div className="navbar-right">
-        <img src={logo} alt="Logo" className="navbar-logo" />
+        <div className="navbar-logo-container">
+          <img src={logo} alt="Logo" className="navbar-logo" />
+        </div>
       </div>
       <div className="navbar-toggle" onClick={toggleMenu}>
         <FaBars />
@@ -64,6 +32,7 @@ const Navbar = () => {
           Contact
         </Link>
       </div>
+
     </nav>
   );
 };
